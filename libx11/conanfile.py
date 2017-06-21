4278f7348da5ba5de4fa9900dc61857838e07e0a
James Choa <triggerhappyelite@gmail.com>
2017-06-21 16:00:15 +0800

    Improve libx11 package
    Add library requirements and deal with static linking

--- libx11/conanfile.py
+++ libx11/conanfile.py
@@ -18,12 +18,24 @@ class Libx11Conan(ConanFile):
         self.run("curl -JOL " + pkgLink)
         self.run("tar xf libX11-{version}.tar.bz2".format(version=self.version))
 
+    def configure(self):
+        self.requires("libxcb/1.12@trigger-happy/stable")
+        self.requires("xproto/7.0.31@trigger-happy/stable")
+        self.requires("inputproto/2.3.2@trigger-happy/stable")
+        self.options["xproto/7.0.13"].shared = self.options.shared
+        self.options["libxcb/1.12"].shared = self.options.shared
+        self.options["inputproto/2.3.2"].shared = self.options.shared
+
     def build(self):
         envBuild = AutoToolsBuildEnvironment(self)
         installPrefix=os.getcwd()
+        sharedFlag = "--disable-shared"
+        if self.options.shared:
+            sharedFlag = "--enable-shared"
+
         with tools.chdir("libX11-{version}".format(version=self.version)):
             with tools.environment_append(envBuild.vars):
-                self.run("./configure --prefix={0} --disable-xf86bigfont".format(installPrefix))
+                self.run("./configure --prefix={0} --disable-xf86bigfont {1}".format(installPrefix, sharedFlag))
                 self.run("make install")
 
     def package(self):
