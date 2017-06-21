2ad1583101728cab520edd64fbefa093248b7870
James Choa <triggerhappyelite@gmail.com>
2017-06-21 16:58:40 +0800

    Improve libxau package
    Dealing with requirements and shared flag

--- libxau/conanfile.py
+++ libxau/conanfile.py
@@ -18,12 +18,20 @@ class LibxauConan(ConanFile):
         self.run("curl -JOL " + pkgLink)
         self.run("tar xf libXau-{version}.tar.bz2".format(version=self.version))
 
+    def configure(self):
+        self.requires("xproto/7.0.31@trigger-happy/stable")
+        self.options["xproto/7.0.31"].shared = self.options.shared
+
     def build(self):
         envBuild = AutoToolsBuildEnvironment(self)
         installPrefix=os.getcwd()
+        sharedFlag = "--disable-shared"
+        if self.options.shared:
+            sharedFlag = "--enable-shared"
+
         with tools.chdir("libXau-{version}".format(version=self.version)):
             with tools.environment_append(envBuild.vars):
-                self.run("./configure --prefix={0}".format(installPrefix))
+                self.run("./configure --prefix={0} {1}".format(installPrefix, sharedFlag))
                 self.run("make install")
 
     def package(self):
