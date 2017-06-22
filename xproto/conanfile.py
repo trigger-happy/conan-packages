9993909ebcadd4895d15a574da2448c4a5f87217
James Choa <triggerhappyelite@gmail.com>
2017-06-22 15:55:14 +0800

    Revise download and build method for xproto

--- xproto/conanfile.py
+++ xproto/conanfile.py
@@ -1,4 +1,6 @@
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
+import urllib3
+import tarfile
 import os
 
 
@@ -14,16 +16,27 @@ class XprotoConan(ConanFile):
     generators = "cmake"
 
     def source(self):
-        pkgLink = 'https://xorg.freedesktop.org/releases/individual/proto/xproto-{version}.tar.bz2'.format(version=self.version)
-        self.run("curl -JOL " + pkgLink)
-        self.run("tar xf xproto-{version}.tar.bz2".format(version=self.version))
+        pkgLink = 'https://xorg.freedesktop.org/releases/individual/proto/xproto-{0}.tar.bz2'.format(self.version)
+        fileName = 'xproto-{0}.tar.bz2'.format(self.version)
+        pool = urllib3.PoolManager()
+        request = pool.request('GET', pkgLink)
+        if request.status == 200:
+            f = open(fileName, 'w')
+            f.write(request.data)
+            f.close()
+            tf = tarfile.open(fileName)
+            tf.extractall('.')
+            tf.close()
+        else:
+            raise Exception('Could not download source file')
 
     def build(self):
         envBuild = AutoToolsBuildEnvironment(self)
         installPrefix=os.getcwd()
-        with tools.chdir("xproto-{version}".format(version=self.version)):
+        shared = "--enable-shared" if self.options.shared else "--disable-shared"
+        with tools.chdir("xproto-{0}".format(self.version)):
             with tools.environment_append(envBuild.vars):
-                self.run("./configure --prefix={0}".format(installPrefix))
+                self.run("./configure --prefix={0} {1}".format(installPrefix, shared))
                 self.run("make install")
 
     def package(self):
